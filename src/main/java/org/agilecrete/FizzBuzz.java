3afedf0bb0b8e20be175444e25ef57c21988db0a
Yorgos Saslis <g.saslis@gmail.com>
2017-09-22 08:24:54 +0300

    Created FizzBuzz skeleton

---
+++ src/main/java/org/agilecrete/FizzBuzz.java
@@ -0,0 +1,6 @@
+package org.agilecrete;
+
+public class FizzBuzz
+{
+
+}
---
+++ src/test/java/org/agilecrete/FizzBuzzTest.java
@@ -0,0 +1,16 @@
+package org.agilecrete;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class FizzBuzzTest {
+
+    @Test
+    public void junitWorks(){
+        Assert.assertTrue(true);
+    }
+
+
+}
