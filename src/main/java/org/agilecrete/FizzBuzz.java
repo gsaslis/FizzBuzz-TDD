de9b6b028f619220c5ea1a2c9d638f8ff2229c5b
Yorgos Saslis <g.saslis@gmail.com>
2017-09-30 13:03:29 +0300

    Added one solution to the Kata
    This is just a simple example of a possible solution, which was created after one kata iteration.

--- src/main/java/org/agilecrete/FizzBuzz.java
+++ src/main/java/org/agilecrete/FizzBuzz.java
@@ -3,4 +3,25 @@ package org.agilecrete;
 public class FizzBuzz
 {
 
+    static final String FIZZ = "fizz";
+    static final String BUZZ = "buzz";
+
+    public String convert(int number) {
+        if (isDivisibleByThree(number) && isDivisibleByFive(number)) {
+            return FIZZ + BUZZ;
+        } else if (isDivisibleByFive(number)) {
+            return BUZZ;
+        } else if (isDivisibleByThree(number)) {
+            return FIZZ;
+        }
+        return Integer.toString(number);
+    }
+
+    private boolean isDivisibleByFive(int number) {
+        return number % 5 == 0;
+    }
+
+    private boolean isDivisibleByThree(int number) {
+        return number % 3 == 0;
+    }
 }
--- src/test/java/org/agilecrete/FizzBuzzTest.java
+++ src/test/java/org/agilecrete/FizzBuzzTest.java
@@ -2,14 +2,47 @@ package org.agilecrete;
 
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class FizzBuzzTest {
 
+    private FizzBuzz fb = null;
+
+    @Before
+    public void setup(){
+        fb = new FizzBuzz();
+    }
+
     @Test
     public void junitWorks(){
         Assert.assertTrue(true);
+    }
+
+    @Test
+    public void whenInputIsTwo_itWillReturnTwo(){
+        Assert.assertEquals("2", fb.convert(2));
+    }
+
+    @Test
+    public void whenInputIsThree_itWillReturnFizz(){
+        Assert.assertEquals(FizzBuzz.FIZZ, fb.convert(3));
+    }
+
+    @Test
+    public void whenInputIsSix_itWillReturnFizz(){
+        Assert.assertEquals(FizzBuzz.FIZZ, fb.convert(6));
+    }
+
+    @Test
+    public void whenInputIsTen_itWillReturnBuzz(){
+        Assert.assertEquals(FizzBuzz.BUZZ, fb.convert(10));
+    }
+
+    @Test
+    public void whenInputIsFifteen_itWillReturnFizzBuzz(){
+        Assert.assertEquals(FizzBuzz.FIZZ + FizzBuzz.BUZZ, fb.convert(15));
     }
 
 
